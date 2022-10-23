fb3d2eba993dd282ab80ce8320c9e646a0adfed8
skymoderator <williamchoi12175@gmail.com>
2022-10-23 19:30:59 +0800

    Initial Commit

---
+++ FoodAssistant/FoodAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodAssistantApp.swift
+//  FoodAssistant
+//
+//  Created by Choi Wai Lap on 23/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct FoodAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
